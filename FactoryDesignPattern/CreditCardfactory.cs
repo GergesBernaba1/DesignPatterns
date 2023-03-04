379243ce0d1dee473b1fdf677c9923453004f2a6
GergesBernaba1 <gergesbernaba2@gmail.com>
2023-03-04 12:03:42 +0200

    FactoryDesignPattren

---
+++ FactoryDesignPattern/CreditCardfactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignPattern
+{
+    public class CreditCardfactory
+    {
+        public static ICreditCard GetCreditCard(string cardType)
+        {
+            ICreditCard cardDetails = null;
+            if (cardType == "MoneyBack")
+            {
+                cardDetails = new MoneyBack();
+            }
+            else if (cardType == "Titanium")
+            {
+                cardDetails = new TitaniumCard();
+            }
+            else if (cardType == "Platinum")
+            {
+                cardDetails = new PlatinumCard();
+            }
+            return cardDetails;
+        }
+    }
+}
---
+++ FactoryDesignPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+using FactoryDesignPattern;
+
+ICreditCard cardDetails = CreditCardfactory.GetCreditCard("Platinum");
+
+if (cardDetails != null)
+{
+    Console.WriteLine("CardType : " + cardDetails.GetCardType());
+    Console.WriteLine("CreditLimit : " + cardDetails.GetCreditLimit());
+    Console.WriteLine("AnnualCharge :" + cardDetails.GetAnnualCharge());
+}
+else
+{
+    Console.Write("Invalid Card Type");
+}
+Console.ReadLine();
+
+
